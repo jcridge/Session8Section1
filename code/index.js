a1d6d17c2d1e200988696ca178260ef346d6f562
jcridge <crij1_12@uni.worc.ac.uk>
2014-11-28 11:20:26 +0000

    Change Structure

--- code/index.js
+++ code/index.js
@@ -1,15 +1,10 @@
 //when the jQuery Mobile page is initialised
 $(document).on('pageinit', function() {
-	
-	//set up listener for button click
+	//set up listener for toggle switch
 	$(document).on('slidestart', getPosition);
-	
-	//change time box to show message
-	$('#time').val("Press the button to get location data");
-	
 });
 
-//Call this function when you want to get the current position
+
 function getPosition() {
 	//change time box to show updated message
 	$('#time').val("Getting data...");
@@ -19,18 +14,15 @@ function getPosition() {
 	//instruct location service to get position with appropriate callbacks
 	var watchID = navigator.geolocation.watchPosition(successPosition,  failPosition, locationOptions);
 	
-	/*var locationOptions = {
+	var locationOptions = {
 		maximumAge: 10000,
 		timeout: 6000,
 		enableHighAccuracy: true
-};*/
-//called when the position is successfully determined
+	};
+}
+
 function successPosition(position) {
 	
-	//You can find out more details about what the position obejct contains here:
-	// http://www.w3schools.com/html/html5_geolocation.asp
-	
-	//lets get some stuff out of the position object
 	var time = position.timestamp;
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
@@ -40,7 +32,7 @@ function successPosition(position) {
 	var speed = position.coords.speed;	
 	
 	//OK. Now we want to update the display with the correct values
-	$('#time').val("Recieved data at " + time);
+	$('#time').val("Received data at " + time);
 	$('#lattext').val(latitude);
 	$('#longtext').val(longitude);
 	$('#altitude').val(altitude);
@@ -51,11 +43,9 @@ function successPosition(position) {
 	
 }
 
-//called if the position is not obtained correctly
 function failPosition(error) {
 	//change time box to show updated message
 	$('#time').val("Error getting data: " + error);
 	$('#latitude').val("Error getting data: " + error);
 	$('#longitude').val("Error getting data: " + error);
 }
-};
